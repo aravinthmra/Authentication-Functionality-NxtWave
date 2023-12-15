e702713d0417f19dbfef29e28b43794cbc61dc01
aravinthmra <aravinthmspv@gmail.com>
2023-12-15 12:59:22 +0000

    V1.0

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom'
+
+const Header = () => (
+  <div>
+    <Link to="/">
+      <div>Home</div>
+    </Link>
+    <Link to="/about">
+      <div>About</div>
+    </Link>
+  </div>
+)
+
+export default Header
